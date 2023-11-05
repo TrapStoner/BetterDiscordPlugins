c45f6fdeb14c916dfa54b827f96d084ab37da6ed
TrapStoner <andy_larkin9@abv.bg>
2023-11-05 03:00:18 +0200

    fix for latest discord

--- HQCallerAvatar/HQCallerAvatar.plugin.js
+++ HQCallerAvatar/HQCallerAvatar.plugin.js
@@ -2,15 +2,15 @@
  * @name HQ Caller Avatar
  * @author TrapStoner#9526
  * @description Makes low quality avatars in call pop-up high quality.
- * @version 1.0
+ * @version 1.1
  * @source https://raw.githubusercontent.com/TrapStoner/BetterDiscordPlugins/master/HQCallerAvatar/HQCallerAvatar.plugin.js
  */
 
 module.exports = meta => {
-	const app = document.querySelector('div[class*=app-]>div[class*=app-]');
+	const app = document.querySelector('div[class*=app_]>div[class*=app_]');
 	function wrapper(mutation) {
-		if (mutation.addedNodes[0] && mutation.addedNodes[0].className == 'wrapper-3X0c60') {
-			const callAvatar = mutation.addedNodes[0].querySelector('div[class*=callAvatarMaskContainer-]');
+		if (mutation.addedNodes[0] && mutation.addedNodes[0].className == 'wrapper_daf1cf') {
+			const callAvatar = mutation.addedNodes[0].querySelector('div[class*=callAvatarMaskContainer_]');
 			callAvatar.querySelector("img").src = callAvatar.querySelector("img").src.replace(/(?:\?size=\d{2,4})?$/, "?size=4096");
 		}
 	};
