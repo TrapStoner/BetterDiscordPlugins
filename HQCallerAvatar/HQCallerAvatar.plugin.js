0075ab21ef3357f4d0a12eb686acc61a91265315
TrapStoner <andy_larkin9@abv.bg>
2024-06-20 08:52:46 +0300

    1.2
    removed raw class selection

--- HQCallerAvatar/HQCallerAvatar.plugin.js
+++ HQCallerAvatar/HQCallerAvatar.plugin.js
@@ -2,14 +2,14 @@
  * @name HQ Caller Avatar
  * @author TrapStoner#9526
  * @description Makes low quality avatars in call pop-up high quality.
- * @version 1.1
+ * @version 1.2
  * @source https://raw.githubusercontent.com/TrapStoner/BetterDiscordPlugins/master/HQCallerAvatar/HQCallerAvatar.plugin.js
  */
 
 module.exports = meta => {
-	const app = document.querySelector('div[class*=app_]>div[class*=app_]');
+	const app = document.querySelector('div[class*=appAsidePanelWrapper_]');
 	function wrapper(mutation) {
-		if (mutation.addedNodes[0] && mutation.addedNodes[0].className == 'wrapper_daf1cf') {
+		if (mutation.addedNodes[0] && mutation.addedNodes[0].classList.value.includes("wrapper_") && mutation.addedNodes[0].childNodes[0].classList.value.includes("root_")) {
 			const callAvatar = mutation.addedNodes[0].querySelector('div[class*=callAvatarMaskContainer_]');
 			callAvatar.querySelector("img").src = callAvatar.querySelector("img").src.replace(/(?:\?size=\d{2,4})?$/, "?size=4096");
 		}
