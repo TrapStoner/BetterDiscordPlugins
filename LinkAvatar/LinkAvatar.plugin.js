362fc4ca01c4431778c455d96deb920224f3c8b2
TrapStoner <andy_larkin9@abv.bg>
2024-07-29 20:41:45 +0300

    1.1

--- LinkAvatar/LinkAvatar.plugin.js
+++ LinkAvatar/LinkAvatar.plugin.js
@@ -2,13 +2,13 @@
  * @name LinkAvatar
  * @author TrapStoner#9526
  * @description Lets you open a user's avatar in the browser.
- * @version 1.0
+ * @version 1.1
  * @source https://github.com/TrapStoner/BetterDiscordPlugins/tree/main/LinkAvatar
  */
 
 module.exports = meta => {
 	function LinkAvatar({target}) {
-		if (target.parentElement.classList.value.includes("avatar_") && target.parentElement.parentElement.classList.value.includes("headerInner_")) {
+		if (target.parentElement?.classList.value.includes("avatar_") && target.parentElement?.parentElement?.classList.value.includes("headerInner_")) {
 			window.open(target.firstChild.querySelector("img").src.replace(/\?.*$/, "?quality=lossless&size=4096"));
 		}
 	};
